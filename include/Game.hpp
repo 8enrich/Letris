f7cc08fa0be8a0fc7951b297cf526bc89db4024f
dylanabzr <dylanab@pm.me>
2024-04-04 14:21:05 -0300

    mudancinhas

--- include/Game.hpp
+++ include/Game.hpp
@@ -2,7 +2,9 @@
 #include <string>
 class Game{
   public:
-    Game(int width , int height, std::string title);
+    Game(int width , int height, std::string title);	
+    Game(const Game& other) = delete;
+	  Game& operator=(const Game& other) = delete;
     ~Game() noexcept;
     bool GameShouldClose() const;
     void Tick();
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,13 @@
 #include "../include/Game.hpp"
 #include <raylib.h>
+#include <assert.h>
 Game::Game(int width, int height, std::string title){
+  assert(!GetWindowHandle()); // Impede que sejam abertas duas janelas. caso ocorra, o programa fecha.
   InitWindow(width, height, title.c_str()); 
 }
 
 Game::~Game() noexcept{
+  assert(GetWindowHandle()); // Impede que feche uma janela que não existe. caso ocorra, o programa fehca
   CloseWindow();
 }
 bool Game::GameShouldClose() const{
@@ -19,7 +22,7 @@ void Game::Tick(){
 }
 
 void Game::Draw(){
-
+  ClearBackground(RAYWHITE);
 }
 
 void Game::Update(){
