23bdd842c0f347070bd97a86fd06648719751165
dylanabzr <dylanab@pm.me>
2024-04-15 17:05:01 -0300

    correçao

--- include/Vec2.hpp
+++ include/Vec2.hpp
@@ -14,26 +14,26 @@ public:
   void SetX(T x_in) {x = x_in;}
   void SetY(T y_in) {y = y_in;}
 public:
-  bool operator>=(const int rhs) const {return x >= rhs && y >= rhs;} 
+  bool operator>=(const T rhs) const {return x >= rhs && y >= rhs;} 
   bool operator>=(const Vec2& rhs) const {return x >= rhs.x && y >= rhs.y;} 
   bool operator>(const Vec2& rhs) const {return x > rhs.x && y > rhs.y;} 
-  bool operator<=(const int rhs) const {return x <= rhs && y <= rhs;} 
+  bool operator<=(const T rhs) const {return x <= rhs && y <= rhs;} 
   bool operator<=(const Vec2& rhs) const {return x <= rhs.x && y <= rhs.y;} 
   bool operator<(const Vec2& rhs) const {return x < rhs.x && y < rhs.y;} 
   bool operator==(const Vec2& rhs) const { return (x == rhs.x) && (y == rhs.y); }
   bool operator!=(const Vec2& rhs) const { return !(*this == rhs); }
   
   Vec2 operator+(const Vec2& rhs) const{ return {x + rhs.x, y + rhs.y}; }
-  Vec2 operator+(const int rhs) const { return {x + rhs, y + rhs}; } 
+  Vec2 operator+(const T rhs) const { return {x + rhs, y + rhs}; } 
   Vec2& operator+=(const Vec2& rhs){ *this = *this + rhs; }
   
   Vec2 operator-(const Vec2& rhs) const { return {x - rhs.x, y - rhs.y}; }
-  Vec2 operator-(const int rhs) const { return {x - rhs, y - rhs}; } 
+  Vec2 operator-(const T rhs) const { return {x - rhs, y - rhs}; } 
   Vec2& operator-=(const Vec2& rhs){ *this = *this - rhs; }
 
   Vec2 operator*(const Vec2& rhs) const { return {x * rhs.x, y * rhs.y}; }
 
-  Vec2 operator*(const int rhs) const { return {x * rhs, y * rhs}; }
+  Vec2 operator*(const T rhs) const { return {x * rhs, y * rhs}; }
 
   Vec2& operator*=(const Vec2& rhs){ *this = *this * rhs; }
 
